aac6440f6c94ace06c94fc63bce2dd70c6f456e4
Marcelo i9 <marcelo@i9criacoes.com.br>
2026-07-14 03:05:43 -0300

    tweak: volume da musica de fundo ainda mais baixo (0.05)

--- src/components/MusicToggle.tsx
+++ src/components/MusicToggle.tsx
@@ -14,7 +14,7 @@ export default function MusicToggle() {
   useEffect(() => {
     const audio = new Audio('/criancas.mp3')
     audio.loop = true
-    audio.volume = 0.08
+    audio.volume = 0.05
     audio.preload = 'auto'
     audioRef.current = audio
 
